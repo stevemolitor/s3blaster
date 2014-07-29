216da56d407336227c911375081d9f24e1e891d9
Steve Molitor <stevemolitor@gmail.com>
2014-07-29 10:04:17 -0500

    add GET example

--- usage.js
+++ usage.js
@@ -1,23 +1,10 @@
-var USAGE =
-    's3blaster OPERATION [parameters] [options]\n' +
-    '\n' +
-    'Operations:\n' +
-    '  PUT {local folder} {bucket} [destination folder]:\n' +
-    '    recursively put local files and directories into S3. Destination folder is optional.\n' +
-    '  GET {s3bucket} {s3key} [local folder]:\n' +
-    '    recursively get folder and/or file from S3 and place in destination folder (defaults to current directory).\n' +
-    '  LIST {s3bucket} [prefix]:\n' +
-    '    List objects in S3 bucket starting with prefix, or all objects if no prefix.\n' +
-    '  DELETE {s3bucket} [prefix]:\n' +
-    '    Delete all objects in S3 bucket starting with prefix, or all objects if no prefix.\n';
-
 module.exports = {
   synopsis: 's3blaster OPERATION parameters [options]',
   operations: {
     PUT: {
       required: ['local_folder', 'bucket'],
-      optional: ['destination_folder'],
-      description: 'recursively put local files and directories into S3. Destination folder is optional.'
+      optional: ['key'],
+      description: 'recursively put local files and directories into S3 at key (folder or file).'
     },
     GET: {
       required: ['bucket', 'key'],
@@ -52,15 +39,19 @@ module.exports = {
   },
   examples: [
     [
-      's3blaster -v PUT folder s3bucket s3folder',
-      'Recursively put all files and folders in local folder to s3bucket in s3folder, in verbose mode.'
+      's3blaster -v PUT local_folder my-s3-bucket my-s3-key',
+      'Recursively put all files and folders in local_folder to my-s3-bucket / my-s3-key (file or folder), in verbose mode.'
     ],
     [
-      's3blaster LIST s3bucket folder1/folder2',
+      's3blaster GET my-s3-bucket my-s3-key local_folder',
+      'Recursively get all files and folders from S3 at my-s3-key (folder or file) to local_folder.'
+    ]
+    [
+      's3blaster LIST my-s3-bucket folder1/folder2',
       "List top level objects starting with prefix 'folder1/folder2'."
     ],
     [
-      's3blaster DELETE s3bucket folder1/folder2',
+      's3blaster DELETE my-s3-bucket folder1/folder2',
       "Delete all objects starting with prefix 'folder1/folder2'."
     ]
   ]
